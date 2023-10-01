d7f63ad132a2bcd30c69ea05d454dc0ebc31eb6e
Souvik Ghosh <souvikghosh2701@gmail.com>
2023-10-01 10:46:27 +0530

    PhpMailer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,33 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'phpmailer/phpmailer' => 
+    array (
+      'pretty_version' => 'v6.8.1',
+      'version' => '6.8.1.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'e88da8d679acc3824ff231fdc553565b802ac016',
+    ),
+  ),
+);
